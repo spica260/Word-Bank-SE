a1150650fbbb982ef9a1b6dcaf1fefa94d97295f
Maria P <mariagpirela@gmail.com>
2024-06-20 12:06:47 -0500

    Api Key
    The Api Key was created at Giphy.com, and is added to the code so it can be easily replaced. New comments added to explain what certain code does. And error catch was added at the end of the fetch. The function was called.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-let apiKey = "";
+let apiKey = "o46suj18PSD39HljljlIrxYOkV9sDao4";
 let limitImages = 12;
 
 function getGiphy() {
   document.querySelector("#btnSearch").addEventListener("click", page => {
-    page.preventDefault(); 
-    document.querySelector('.container').innerHTML = "";
+    page.preventDefault(); // Prevent the page to reload
+    document.querySelector('.container').innerHTML = ""; // Clears the container for every user search
 
     let url = `https://api.giphy.com/v1/text/search?api_key=${apiKey}&limit=${limitImages}&q=`;
     let userSearch = document.querySelector("#search").value.trim();
     url = url + userSearch;
 
-      fetch(url).then(response => response.json()).then(content => {
-      for (let i = 0; i < content.data.length; i++) {
-        console.log(content.data);
-        let img = document.createElement("img");
-        img.src = content.data[i].images.original.url;
-        let out = document.querySelector(".container");
-        out.appendChild(img);
-      }
-        document.querySelector("#search").value = "";
+    // Fetch Giphy with url previously created
+        fetch(url).then(response => response.json()).then(content => {
+            for (let i = 0; i < content.data.length; i++) {
+            console.log(content.data);
+            let img = document.createElement("img");
+            img.src = content.data[i].images.original.url;
+            let out = document.querySelector(".container");
+            out.appendChild(img);
+        }
+        document.querySelector("#search").value = ""; // Clears the input for every user search
     })
+    // Catch any error that may occur
+    .catch(bug => {
+        console.error(bug);
+      });
   });
 }
+
+getGiphy(); // Call the function
